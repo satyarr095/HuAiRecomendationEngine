5ec4ba631d76c647af4459deb65fd31ba884b1bd
satyarr95 <satyarr96@gmail.com>
2025-06-16 17:03:01 +0530

    Initial commit: Add recommendation engine project structure

--- server/src/index.js
+++ server/src/index.js
@@ -3,10 +3,8 @@ import { configDotenv } from 'dotenv';
 import { connectDB } from '../config/db.js';
 configDotenv();
 const app = express();
-
 const PORT = process.env.PORT || 8080;
 app.use(express.json());
-
 app.listen(PORT, () => {
     connectDB();
     console.log(`Server is running on port ${PORT}`);
