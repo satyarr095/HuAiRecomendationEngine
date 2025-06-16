330e73a864f75257ae1f35a8817945a34d2b7e53
Satya m <satyam@Satyas-MacBook-Pro.local>
2025-06-16 16:30:13 +0530

    Initial commit

---
+++ server/src/index.js
@@ -0,0 +1,13 @@
+import express from 'express';
+import { configDotenv } from 'dotenv';
+import { connectDB } from '../config/db.js';
+
+configDotenv();
+const app = express();
+const PORT = process.env.PORT || 8080;
+app.use(express.json());
+
+app.listen(PORT, () => {
+    connectDB();
+    console.log(`Server is running on port ${PORT}`);
+});
